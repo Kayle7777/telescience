d6cdad1c4f029b1abdf984d6c2362ac8bba173be
Kayle7777 <jessewebb2@gmail.com>
2018-12-25 10:18:00 -0600

    At least got images displaying on page now

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
-const Main = () => {
-    return <div />;
+const Main = props => {
+    const [imgUrls, pushUrls] = useState([]);
+
+    useEffect(() => {
+        pushUrls(genUrls());
+    }, []);
+
+    console.log(imgUrls);
+    console.log(process.env.PUBLIC_URL);
+    return (
+        <div>
+            {imgUrls.map(url => {
+                return <img key={url} alt={url} src={require(`../../public/images/maps/cogmap1/${url}.png`)} />;
+            })}
+        </div>
+    );
+
+    function genUrls() {
+        const arr = [];
+        for (let i = 0; i < 8; i++) {
+            for (let g = 0; g < 8; g++) {
+                arr.push(`${i},${g}`);
+            }
+        }
+        return arr;
+    }
 };
 
-export default Main;
+export default withStyles({})(Main);
