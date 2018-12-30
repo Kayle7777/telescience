9a705c04329b2333d73e4e5a79168d485339bb09
Kayle7777 <jessewebb2@gmail.com>
2018-12-29 23:29:05 -0600

    Make the do telescience math thing collapsible

--- src/components/DoMath.js
+++ src/components/DoMath.js
@@ -7,6 +7,7 @@ import {
     TableHead,
     TableRow,
     Paper,
+    Collapse,
     TextField,
     InputAdornment,
     Typography,
@@ -16,137 +17,138 @@ const styles = theme => ({
     textField: {
         width: 75,
     },
-    doMathPos: {
+    main: {
         zIndex: 2,
         position: 'fixed',
         marginLeft: theme.spacing.unit,
         marginTop: theme.spacing.unit,
-        marginBottom: theme.spacing.unit * 2,
         maxWidth: 600 + theme.spacing.unit * 8,
     },
     resultsPos: {
         zIndex: 2,
-        position: 'fixed',
+        position: 'relative',
+        marginTop: theme.spacing.unit * 2,
         width: 249,
-        marginLeft: theme.spacing.unit,
-        marginTop: 3 * theme.spacing.unit + 281,
     },
 });
 
 const DoMath = props => {
     const { classes, selectedTile } = props;
     const [gpsValues, setValue] = useState({ input: [100, 50], actualX: [6, 7], actualY: [49, 51] });
+    const [mathIn, toggleMath] = useState(true);
     const xDivisor = gpsValues.actualX[1] - gpsValues.actualX[0],
         yDivisor = gpsValues.actualY[1] - gpsValues.actualY[0],
         xModifier = gpsValues.input[0] - gpsValues.actualX[0] / xDivisor,
         yModifier = gpsValues.input[1] - gpsValues.actualY[0] / yDivisor;
 
     return (
-        <>
-            <Paper className={classes.doMathPos}>
-                <Typography variant="overline" align="center" id="tableTitle">
-                    Do telescience math
-                </Typography>
-                <Table padding="dense">
-                    <TableHead>
-                        {/* HEADER */}
-                        <TableRow>
-                            <TableCell align="center">Console coord</TableCell>
-                            <TableCell align="center">GPS coord</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {/* CONSOLE X1 // ACTUAL X1 */}
+        <div className={classes.main}>
+            <Collapse in={mathIn} collapsedHeight={'31px'}>
+                <Paper>
+                    <Typography onClick={() => toggleMath(!mathIn)} variant="overline" align="center" id="tableTitle">
+                        Do telescience math
+                    </Typography>
+                    <Table padding="dense">
+                        <TableHead>
+                            {/* HEADER */}
+                            <TableRow>
+                                <TableCell align="center">Console coord</TableCell>
+                                <TableCell align="center">GPS coord</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {/* CONSOLE X1 // ACTUAL X1 */}
 
-                        <TableRow>
-                            <TableCell>
-                                <TextField
-                                    className={classes.textField}
-                                    onChange={e => update(e, 'input', 0)}
-                                    value={gpsValues.input[0]}
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position="start">X1</InputAdornment>,
-                                    }}
-                                />
-                            </TableCell>
-                            <TableCell>
-                                <TextField
-                                    className={classes.textField}
-                                    onChange={e => update(e, 'actualX', 0)}
-                                    value={gpsValues.actualX[0]}
-                                />
-                            </TableCell>
-                        </TableRow>
+                            <TableRow>
+                                <TableCell>
+                                    <TextField
+                                        className={classes.textField}
+                                        onChange={e => update(e, 'input', 0)}
+                                        value={gpsValues.input[0]}
+                                        InputProps={{
+                                            startAdornment: <InputAdornment position="start">X1</InputAdornment>,
+                                        }}
+                                    />
+                                </TableCell>
+                                <TableCell>
+                                    <TextField
+                                        className={classes.textField}
+                                        onChange={e => update(e, 'actualX', 0)}
+                                        value={gpsValues.actualX[0]}
+                                    />
+                                </TableCell>
+                            </TableRow>
 
-                        {/* CONSOLE Y1 // ACTUAL Y1 */}
+                            {/* CONSOLE Y1 // ACTUAL Y1 */}
 
-                        <TableRow>
-                            <TableCell>
-                                <TextField
-                                    className={classes.textField}
-                                    onChange={e => update(e, 'input', 1)}
-                                    value={gpsValues.input[1]}
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position="start">Y1</InputAdornment>,
-                                    }}
-                                />
-                            </TableCell>
-                            <TableCell>
-                                <TextField
-                                    className={classes.textField}
-                                    onChange={e => update(e, 'actualY', 0)}
-                                    value={gpsValues.actualY[0]}
-                                />
-                            </TableCell>
-                        </TableRow>
+                            <TableRow>
+                                <TableCell>
+                                    <TextField
+                                        className={classes.textField}
+                                        onChange={e => update(e, 'input', 1)}
+                                        value={gpsValues.input[1]}
+                                        InputProps={{
+                                            startAdornment: <InputAdornment position="start">Y1</InputAdornment>,
+                                        }}
+                                    />
+                                </TableCell>
+                                <TableCell>
+                                    <TextField
+                                        className={classes.textField}
+                                        onChange={e => update(e, 'actualY', 0)}
+                                        value={gpsValues.actualY[0]}
+                                    />
+                                </TableCell>
+                            </TableRow>
 
-                        {/* CONSOLE X2 // ACTUAL X2 */}
+                            {/* CONSOLE X2 // ACTUAL X2 */}
 
-                        <TableRow>
-                            <TableCell>
-                                <TextField
-                                    className={classes.textField}
-                                    key="gpsX1"
-                                    value={gpsValues.input[0] + 1}
-                                    disabled
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position="start">X2</InputAdornment>,
-                                    }}
-                                />
-                            </TableCell>
-                            <TableCell>
-                                <TextField
-                                    className={classes.textField}
-                                    onChange={e => update(e, 'actualX', 1)}
-                                    value={gpsValues.actualX[1]}
-                                />
-                            </TableCell>
-                        </TableRow>
+                            <TableRow>
+                                <TableCell>
+                                    <TextField
+                                        className={classes.textField}
+                                        key="gpsX1"
+                                        value={gpsValues.input[0] + 1}
+                                        disabled
+                                        InputProps={{
+                                            startAdornment: <InputAdornment position="start">X2</InputAdornment>,
+                                        }}
+                                    />
+                                </TableCell>
+                                <TableCell>
+                                    <TextField
+                                        className={classes.textField}
+                                        onChange={e => update(e, 'actualX', 1)}
+                                        value={gpsValues.actualX[1]}
+                                    />
+                                </TableCell>
+                            </TableRow>
 
-                        {/* CONSOLE Y2 // ACTUAL Y2 */}
+                            {/* CONSOLE Y2 // ACTUAL Y2 */}
 
-                        <TableRow>
-                            <TableCell>
-                                <TextField
-                                    className={classes.textField}
-                                    value={gpsValues.input[1] + 1}
-                                    disabled
-                                    InputProps={{
-                                        startAdornment: <InputAdornment position="start">Y2</InputAdornment>,
-                                    }}
-                                />
-                            </TableCell>
-                            <TableCell>
-                                <TextField
-                                    className={classes.textField}
-                                    value={gpsValues.actualY[1]}
-                                    onChange={e => update(e, 'actualY', 1)}
-                                />
-                            </TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </Paper>
+                            <TableRow>
+                                <TableCell>
+                                    <TextField
+                                        className={classes.textField}
+                                        value={gpsValues.input[1] + 1}
+                                        disabled
+                                        InputProps={{
+                                            startAdornment: <InputAdornment position="start">Y2</InputAdornment>,
+                                        }}
+                                    />
+                                </TableCell>
+                                <TableCell>
+                                    <TextField
+                                        className={classes.textField}
+                                        value={gpsValues.actualY[1]}
+                                        onChange={e => update(e, 'actualY', 1)}
+                                    />
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </Paper>
+            </Collapse>
             <Paper className={classes.resultsPos}>
                 <Typography variant="overline" align="center" id="tableTitle">
                     Real Coordinates
@@ -221,7 +223,7 @@ const DoMath = props => {
                     </TableBody>
                 </Table>
             </Paper>
-        </>
+        </div>
     );
     function update(e, target, index) {
         let val = parseInt(e.target.value);
