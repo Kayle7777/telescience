724a14dade471d96869b7693786f3e5146ce2cda
Kayle7777 <jessewebb2@gmail.com>
2018-12-31 19:44:35 -0600

    Woops, forgot to fix tests import...

--- src/__tests__/Main.test.js
+++ src/__tests__/Main.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-testing-library';
-import Main from './Main';
+import Main from '../pages/Main';
 
 describe('testing Main.js', () => {
     test('it renders', () => {
