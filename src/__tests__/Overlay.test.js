562aa4d2e8b878b861b79870a96d1b6d1e4e48d5
Kayle7777 <jessewebb2@gmail.com>
2019-01-20 15:03:07 -0600

    Tests

--- src/__tests__/Overlay.test.js
+++ src/__tests__/Overlay.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, cleanup } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 import Overlay from '../components/Overlay';
 
 const overlayTestProps = {
