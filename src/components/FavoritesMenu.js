ea0a5d0633e071e099551794f064048e6a0a2911
Jesse Webb <jesse.webb@speakwrite.com>
2019-04-11 15:33:54 -0500

    Fix renaming of favorites to new damn react pattern

--- src/components/FavoritesMenu.js
+++ src/components/FavoritesMenu.js
@@ -37,13 +37,10 @@ const FavoritesMenu = props => {
     const [collapseIn, handleCollapse] = useState(false);
     const [permaCollapse, doPermaCollapse] = useState(false);
 
-    useEffect(
-        () => {
-            if (favorites[selectedMap].length >= 1 && !collapseIn && !permaCollapse) return handleCollapse(true);
-            else if (favorites[selectedMap].length < 1) return handleCollapse(false);
-        },
-        [props]
-    );
+    useEffect(() => {
+        if (favorites[selectedMap].length >= 1 && !collapseIn && !permaCollapse) return handleCollapse(true);
+        else if (favorites[selectedMap].length < 1) return handleCollapse(false);
+    }, [props]);
 
     return (
         <Paper className={classes.paper}>
@@ -75,9 +72,10 @@ const FavoritesMenu = props => {
                                     value={each.name}
                                     onChange={e => {
                                         let val = e.target.value;
+
                                         return modFavorites(favorites => {
                                             favorites[selectedMap][index].name = val;
-                                            return favorites;
+                                            return { ...favorites };
                                         });
                                     }}
                                     FormHelperTextProps={{ style: { textAlign: 'center' } }}
