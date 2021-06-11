6e74d08bd05e7e8dab67648877880a02ae7a8825
Jesse Webb <jesse.webb@claritymis.com>
2021-06-10 22:52:44 -0500

    I should get fired for removing tests

--- src/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-testing-library';
-import App from '../App';
-
-describe('testing App.js', () => {
-    test('it renders', () => {
-        render(<App />);
-    });
-});
